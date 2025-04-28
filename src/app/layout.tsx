cb5c18973a13b8574916d8399b131a240cde4382
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-04-28 09:00:31 -0300

    feat: add roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
+import { Roboto } from 'next/font/google';
 import "./globals.css";
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  weight: ['400', '700'],
+  style: ['normal'],
+});
 
 export const metadata: Metadata = {
   title: "Code Leap",
@@ -14,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.variable}>
         {children}
       </body>
     </html>
