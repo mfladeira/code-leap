4d49fbd7216a95dd6a19472402c7c57976bf74de
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-04-29 16:08:54 -0300

    refactor: remove label from input component andd add props classname

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -3,25 +3,20 @@ import styles from './input.module.css';
 export type InputProps = Readonly<{
   placeholder?: string;
   id: string;
-  label: string;
   value?: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 }>
 
 export const Input = (props: InputProps) => {
   return (
-    <div className={styles.inputWrapper}>
-      <label htmlFor={props.id} className={styles.label}>
-        {props.label}
-      </label>
-      <input
-        id={props.id}
-        value={props.value}
-        onChange={props.onChange}
-        type="text"
-        className={styles.input}
-        placeholder={props.placeholder}
-      />
-    </div>
+    <input
+      id={props.id}
+      value={props.value}
+      onChange={props.onChange}
+      type="text"
+      className={`${styles.input} ${props.className}`}
+      placeholder={props.placeholder}
+    />
   )
 }
