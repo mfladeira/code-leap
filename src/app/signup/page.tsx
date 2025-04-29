f21821b21aca7df6b374d13f1f34accf559126d1
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-04-29 19:38:00 -0300

    feat: add signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,19 @@
+import { Card } from '@/components/card/card';
+import styles from './signup.module.css';
+import { Input } from '@/components/input/input';
+import { Label } from '@/components/label/label';
+import { Button } from '@/components/button/button';
+
+export default function SignUp() {
+  return (
+    <div className={styles.signup}>
+      <Card width="500px" title='Welcome to CodeLeap network!'>
+        <Label id='username' text='Please enter your username' className='mb8'></Label>
+        <Input id='username' placeholder='John doe'></Input>
+        <div className={`${styles.buttonWrapper} mt16`}>
+          <Button text='ENTER' variant='primary'></Button>
+        </div>
+      </Card>
+    </div>
+  )
+}
