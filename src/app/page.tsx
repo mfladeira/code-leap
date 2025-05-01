35e50dc9e57ddd405e5ada62615d91b6c2dc51e5
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-05-01 16:44:22 -0300

    feat: add main page with mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,50 @@
+import { Card } from "@/components/card/card";
 import styles from "./page.module.css";
+import { Input } from "@/components/input/input";
+import { Label } from "@/components/label/label";
+import { TextArea } from "@/components/textArea/textArea";
+import { Button } from "@/components/button/button";
+import { Post } from "@/components/post/post";
+import { postMock } from "./postMock";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <main className={styles.home}>
+      <div className={styles.homeWrapper}>
+        <header className={styles.header}>
+          <h2>CodeLeap Network</h2>
+        </header>
+        <div className={styles.content}>
+          <Card title="What’s on your mind?">
+            <Label id="title" text="Title" className="mb8"></Label>
+            <Input id="title" placeholder="Hello world"></Input>
 
-    </div>
+            <Label id="content" text="Content" className="mt24 mb8"></Label>
+            <TextArea id="content" placeholder="Content here" className="mb24"></TextArea>
+
+            <div className={styles.buttonWrapper}>
+              <Button children="Create" variant="primary"></Button>
+            </div>
+          </Card>
+          <div className={styles.postWrapper}>
+            {
+              postMock.map(post => {
+                return (
+                  <Post
+                    key={post.id}
+                    title={post.title}
+                    belongsToUser={post.belongsToUser}
+                    username={post.username}
+                    date={post.date}
+                    children={post.children}
+                  >
+                  </Post>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ src/app/postMock.ts
@@ -0,0 +1,20 @@
+export const postMock = [
+  {
+    "id": 1,
+    "title": "My First Post at CodeLeap Network!",
+    "belongsToUser": true,
+    "username": "@Victor",
+    "date": "2025-04-29 14:30:00",
+    "children": `Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
+
+    Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat.`
+  },
+  {
+    "id": 2,
+    "title": "My Second Post at CodeLeap Network!",
+    "belongsToUser": false,
+    "username": "@Vini",
+    "date": "2025-04-30 14:30:00",
+    "children": `Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.`
+  }
+]
