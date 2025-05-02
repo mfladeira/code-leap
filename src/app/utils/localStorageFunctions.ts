6bce696222ea0b78d4fe2d3ea5f453c574c7c14e
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-05-02 18:37:42 -0300

    feat: add local storage function

---
+++ src/app/utils/localStorageFunctions.ts
@@ -0,0 +1,12 @@
+export const getItem = (itemName: string) => {
+  const item = localStorage.getItem(itemName)
+  return item ? JSON.parse(item) : null
+}
+
+export const removeItem = (itemName: string) => {
+  localStorage.remove(itemName);
+}
+
+export const setItem = (itemName: string, content: string) => {
+  localStorage.setItem(itemName, content);
+}
