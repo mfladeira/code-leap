cb28594794b8fde74201bdbeaf765af427b82b38
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-05-02 18:37:54 -0300

    feat: add redux to signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,46 @@
+"use client";
+
 import { Card } from '@/components/card/card';
 import styles from './signup.module.css';
 import { Input } from '@/components/input/input';
 import { Label } from '@/components/label/label';
 import { Button } from '@/components/button/button';
+import { useAppDispatch } from '../../store/hooks';
+import { useRouter } from 'next/navigation'
+import { login } from '@/store/features/authSlice';
+import { useEffect, useState } from 'react';
+import { getItem, setItem } from '../utils/localStorageFunctions';
 
 export default function SignUp() {
+  const [username, setUsername] = useState('');
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    const username = getItem('username');
+    const isAuthenticated = getItem('isAuthenticated')
+
+    if (Boolean(isAuthenticated) && username) {
+      router.push('/');
+    }
+  }, []);
+
+  const handleLogin = () => {
+    if (!username.trim()) return;
+    dispatch(login(username));
+    setItem('user', username);
+    setItem('isAuthenticated', 'true');
+    router.push('/');
+  }
+
   return (
     <div className={styles.signup}>
       <div className={styles.cardWrapper}>
         <Card title='Welcome to CodeLeap network!'>
           <Label id='username' text='Please enter your username' className='mb8'></Label>
-          <Input id='username' placeholder='John doe'></Input>
+          <Input id='username' placeholder='John doe' value={username} onChange={(e) => setUsername(e.target.value)}></Input>
           <div className={styles.buttonWrapper}>
-            <Button children='ENTER' variant='primary'></Button>
+            <Button children='ENTER' variant='primary' onClick={handleLogin}></Button>
           </div>
         </Card>
       </div>
