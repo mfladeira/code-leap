d6b1fe804ae127a4c1878c8d2a0c1383383399ad
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-05-04 12:13:45 -0300

    feat: add service post - crud

---
+++ src/services/posts.ts
@@ -0,0 +1,46 @@
+export const fetchPosts = async () => {
+  const response = await fetch("https://dev.codeleap.co.uk/careers/");
+  const data = await response.json();
+  return data.results;
+};
+
+export const createPost = async (title: string, content: string, username: string) => {
+  const response = await fetch('https://dev.codeleap.co.uk/careers/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title,
+      content,
+      username
+    }),
+  });
+  const data = await response.json();
+  return data;
+}
+
+export const deletePost = async (id: number) => {
+  const response = await fetch(`https://dev.codeleap.co.uk/careers/${id}/`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response;
+}
+
+export const editPost = async (id: number, title: string, content: string) => {
+  const response = await fetch(`https://dev.codeleap.co.uk/careers/${id}/`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title,
+      content,
+    }),
+  });
+  const data = await response.json();
+  return data;
+}
